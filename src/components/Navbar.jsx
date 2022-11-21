375092ca1403c497f442194c775d7f1d7b3fc66f
Brett <108785869+brettman1@users.noreply.github.com>
2022-11-21 02:45:50 -0600

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,57 @@
+
+
 import React, { useState } from 'react'; 
 import {
   FaBars,
   FaTimes,
   FaGithub,
   FaLinkedin,
+  FaFacebook,
+  FaLinkedinIn,
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
-  const handleClick = () => setNav(!nav)
-  return (
-    <div className='sticky top-0 w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      <div>
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <h1 className=' font-thin text-2xl italic font-serif'></h1>
       </div>
-      <ul className='hidden md:flex gap-x-8 '>
+      {/* menu */}
+      <ul className='hidden md:flex gap-x-8'>
         <li>
-          <Link className='hover:underline' to='home' smooth={true} duration={500}  >
+          <Link className='hover:underline' to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li>
-          <Link className='hover:underline' to='about' smooth={true} duration={500} >
+          <Link className='hover:underline' to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li>
-          <Link className='hover:underline' to='skills' smooth={true} duration={500} >
+          <Link className='hover:underline' to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li>
-          <Link className='hover:underline' to='work' smooth={true} duration={500} >
+          <Link className='hover:underline' to='work' smooth={true} duration={500}>
             Work
           </Link>
         </li>
         <li>
-          <Link className='hover:underline' to='contact' smooth={true} duration={500} >
+          <Link className='hover:underline' to='contact' smooth={true} duration={500}>
             Contact
           </Link>
         </li>
       </ul>
       {/* Hamburger */}
-     <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className='md:hidden z-10'>
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/* Mobile menu */}
@@ -53,15 +59,15 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'sticky top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl hover:underline'>
-          <Link className = 'hover:underline' onClick={handleClick} to='home' smooth={true} duration={500}>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl hover:underline'>
+        <li className='py-6 text-4xl'>
           {' '}
           <Link onClick={handleClick} to='about' smooth={true} duration={500}>
             About
@@ -92,41 +98,33 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              rel='noreferrer'
-              target='_blank'
-              href='https://www.linkedin.com/in/brett-starrett-7965b5182/'
+              href='/'
             >
-              Linkedin <FaLinkedin size={25} />
+              Linkedin <FaLinkedin size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              rel='noreferrer'
-              target='_blank'
-              href='https://github.com/brettman1'
+              href='/'
             >
-              Github <FaGithub size={25} />
+              Github <FaGithub size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              rel='noreferrer'
-              target='_blank'
-              href='mailto: dev.bretts@gmail.com'
+              href='/'
             >
-              Email <HiOutlineMail size={25} />
+              Email <HiOutlineMail size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              rel='noreferrer'
-              target='_blank'
-              href='/public/BrettStarrettResume.PDF'
+              href='/'
             >
-              Resume <BsFillPersonLinesFill size={25} />
+              Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
         </ul>
@@ -135,4 +133,3 @@ const Navbar = () => {
   );
 };
 export default Navbar;
-
